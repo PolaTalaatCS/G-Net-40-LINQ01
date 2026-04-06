2bcba0f008dcc14e866b9425584e55d2ddafc6d9
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-04-06 16:38:52 +0200

    Question 3

--- Program.cs
+++ Program.cs
@@ -8,24 +8,38 @@ namespace G_Net_40_LINQ01
     {
         static void Main(string[] args)
         {
-           #region Question 1
-        //    var productList = Source.ProductList;
+            #region Question 1
+            //    var productList = Source.ProductList;
 
-        //    var seafoodProducts = productList
-        //        .Where(p => p.Category == "Seafood")
-        //        .Select(p => new
-        //        {
-        //            p.ProductName,
-        //            Price = p.UnitPrice
-        //        });
+            //    var seafoodProducts = productList
+            //        .Where(p => p.Category == "Seafood")
+            //        .Select(p => new
+            //        {
+            //            p.ProductName,
+            //            Price = p.UnitPrice
+            //        });
 
-        //    foreach (var product in seafoodProducts)
-        //    {
-        //        Console.WriteLine($"{product.ProductName} - {product.Price}");
-        //    }
+            //    foreach (var product in seafoodProducts)
+            //    {
+            //        Console.WriteLine($"{product.ProductName} - {product.Price}");
+            //    }
             #endregion
+            #region Question 2
+            //var productlist = Source.ProductList;
+            //var productNames = productlist.Select(p => p.ProductName);
 
-
+            //foreach (var name in productNames)
+            //{
+            //    Console.WriteLine($"Product name {name}");
+            //}
+            #endregion
+            #region Question 3
+            //var productlist3 = Source.ProductList.OrderByDescending(p => p.UnitPrice);
+            //foreach (var i in productlist3)
+            //{
+            //    Console.WriteLine($"Name {i.ProductName} price {i.UnitPrice}");
+            //}
+            #endregion
         }
     }
 }
