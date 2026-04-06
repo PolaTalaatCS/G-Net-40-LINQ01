91c6d9cd73ffecbec6a15529d71882521fc0eaae
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-04-06 16:27:50 +0200

    Question 1

--- Program.cs
+++ Program.cs
@@ -8,22 +8,24 @@ namespace G_Net_40_LINQ01
     {
         static void Main(string[] args)
         {
-            #region Question 1
-            var productList = Source.ProductList;
+           #region Question 1
+        //    var productList = Source.ProductList;
 
-            var seafoodProducts = productList
-                .Where(p => p.Category == "Seafood")
-                .Select(p => new
-                {
-                    p.ProductName,
-                    Price = p.UnitPrice
-                });
+        //    var seafoodProducts = productList
+        //        .Where(p => p.Category == "Seafood")
+        //        .Select(p => new
+        //        {
+        //            p.ProductName,
+        //            Price = p.UnitPrice
+        //        });
 
-            foreach (var product in seafoodProducts)
-            {
-                Console.WriteLine($"{product.ProductName} - {product.Price}");
-            }
+        //    foreach (var product in seafoodProducts)
+        //    {
+        //        Console.WriteLine($"{product.ProductName} - {product.Price}");
+        //    }
             #endregion
+
+
         }
     }
 }
