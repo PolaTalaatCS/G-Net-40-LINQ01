dcfed8679a8025eccabe931553a0c7fd7365e41f
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-04-06 16:26:14 +0200

    Add project files.

---
+++ Helper/StringCaseInsensitiveComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_40_LINQ01.Helper
+{
+  
+        internal class StringCaseInsensitiveComparer : IComparer<string>
+        {
+            public int Compare(string? x, string? y)
+            {
+                return string.Compare(x, y, StringComparison.OrdinalIgnoreCase);
+            }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using G_Net_40_LINQ01.Models;
+using G_Net_40_LINQ01.DataSources;
+namespace G_Net_40_LINQ01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+            var productList = Source.ProductList;
+
+            var seafoodProducts = productList
+                .Where(p => p.Category == "Seafood")
+                .Select(p => new
+                {
+                    p.ProductName,
+                    Price = p.UnitPrice
+                });
+
+            foreach (var product in seafoodProducts)
+            {
+                Console.WriteLine($"{product.ProductName} - {product.Price}");
+            }
+            #endregion
+        }
+    }
+}
